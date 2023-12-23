0fbd12d312c9eec11bc27ed1b4edb8e935284d50
MichaelM3 <michael.muniz94@gmail.com>
2023-12-23 11:56:45 -0500

    added module folders for server and http

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
+use http::Request;
+use http::Method;
+use server::Server;
+
+mod server;
+mod http;
+
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on port: {}", self.addr)
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: String,
-    method: String,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    PATCH,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
 }
 
 /*
