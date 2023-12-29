0b82a37056ad3a01284f451cc216f3c9864644c8
MichaelM3 <michael.muniz94@gmail.com>
2023-12-29 17:35:57 -0500

    added traits and type conversions

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -13,5 +16,27 @@ impl Server {
         println!("Listening on port: {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _addr)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!(
+                                "Received a request: {}", 
+                                String::from_utf8_lossy(&buffer)
+                            );
+                            match Request::try_from(&buffer[..]) {
+                                Ok(req) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to estable a connection: {}", e),
+            }
+        }
     }
 }
